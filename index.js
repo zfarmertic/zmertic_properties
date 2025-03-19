f9c624abf76f77d2b8a9e1c7a0b8d83a699d1991
zfarmertic <lilyigbunu@gmail.com>
2025-03-19 10:50:56 +0100

    first changes

---
+++ index.js
@@ -0,0 +1,28 @@
+// let search = document.getElementById("search")
+
+// function searchBarF(){
+//     let searchbar = document.getElementById("addSearchBar")
+//     let existingInput = searchbar.querySelector('input');
+//         if(!existingInput){
+//         const input = document.createElement("input")
+//         input.type = "text"
+//         input.className = "created"
+//         searchbar.appendChild(input)
+//         }
+
+//         else if(existingInput){
+//             existingInput.style.display = (existingInput.style.display === "none") ? "block" : "none";
+//         }
+// }
+
+// search.addEventListener("click", searchBarF)
+
+let ShowMenu = document.getElementById("ShowMenu")
+let sideBar = document.querySelector("#sideBar")
+ShowMenu.addEventListener("click", ()=>{
+        sideBar.style.display = "flex"
+})
+let HideMenu = document.getElementById("HideMenu")
+HideMenu.addEventListener("click", ()=>{
+    sideBar.style.display = "none"
+})
